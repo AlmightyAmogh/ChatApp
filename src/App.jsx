954aee1312acefd8a79656886c5648c240475989
Amogh Shanbhag <amogh2020@gmail.com>
2022-10-30 00:37:10 +0530

    adding through dashboard only but other ui done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import "./App.css";
 import {ChatEngine} from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+  if(!localStorage.getItem('username')) return <LoginForm/>
   return (
     <ChatEngine 
     height = "100vh"
     projectID = "8c29f5b5-16ea-4fcf-87ef-99c4737fb05c"
-    userName = "almightyAmo"
-    userSecret = "123123"
+    userName = {localStorage.getItem('username')}
+    userSecret = {localStorage.getItem('password')}
     renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
     />
   )
